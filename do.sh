eedc655944a38997285f04d71f8b86f02e5e0d35
Aston <aston668334.1@gmail.com>
2024-05-27 13:29:11 -0400

    update

--- do.sh
+++ do.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 遍历目录及其子目录下的所有docker-compose.yml文件
-find /path/to/your/directory -type f -name "docker-compose.yml" |
+find ./ -type f -name "docker-compose.yml" |
 while read file; do
     # 提取目录路径
     dir=$(dirname "$file")
